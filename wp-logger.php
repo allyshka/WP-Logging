9e558ff6e283f8662b5f56603fe9274c986fb5d7
allyshka <allyshka@localhost.localdomain>
2015-04-14 00:08:45 -0400

    Add wpdb class casting to the main class of WP_Logging for properly working with new version of Wordpress (>4.0)

--- wp-logger.php
+++ wp-logger.php
@@ -42,7 +42,7 @@ class WP_Logger extends wpdb {
 	 * Adds all of the filters and hooks and enforces singleton pattern
 	 */
 	function __construct() {
-		global $wbdb;
+		global $wpdb;
 
 		// Enforces a single instance of this class.
 		if ( isset( self::$instance ) ) {
@@ -50,6 +50,9 @@ class WP_Logger extends wpdb {
 		}
 
 		self::$instance = $this;
+
+		// Check of $wpdb object type
+		if($wpdb instanceOf wpdb) $this->cast($this, $wpdb);
 
 		// These actions setup the plugin and inject scripts and styles on our logs page.
 		add_action( 'init',                  array( $this, 'init' ), 1 );
@@ -84,6 +87,36 @@ class WP_Logger extends wpdb {
 			print $this->showLogFile($this->file);
 		}
 	}
+
+        /**
+         * Class casting
+         *
+         * @param string|object $destination
+         * @param object $sourceObject
+         * @return object
+         */
+        function cast($destination, $sourceObject)
+        {
+                if (is_string($destination)) {
+                        $destination = new $destination();
+                }
+                $sourceReflection = new ReflectionObject($sourceObject);
+                $destinationReflection = new ReflectionObject($destination);
+                $sourceProperties = $sourceReflection->getProperties();
+                foreach ($sourceProperties as $sourceProperty) {
+                        $sourceProperty->setAccessible(true);
+                        $name = $sourceProperty->getName();
+                        $value = $sourceProperty->getValue($sourceObject);
+                        if ($destinationReflection->hasProperty($name)) {
+                                $propDest = $destinationReflection->getProperty($name);
+                                $propDest->setAccessible(true);
+                                $propDest->setValue($destination,$value);
+                        } else {
+                                $destination->$name = $value;
+                        }
+                }
+                return $destination;
+        }
 
 	/**
 	 * Will return the current version of WP Logger to plugins that call the `wp_logger_version` filter.
